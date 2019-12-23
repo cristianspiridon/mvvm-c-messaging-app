1967bea4c40df07d094b80aa8737dd8f1c70710f
Cristian Spiridon <cristian.spiridon@gmail.com>
2019-12-23 11:04:51 +0000

    Initial Commit

---
+++ LennyMessaging/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LennyMessaging
+//
+//  Created by The Spiridon's on 23/12/2019.
+//  Copyright © 2019 The Spiridon's. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
