ff8538471492d154f215014c60a48cfccef6bf30
Cristian Spiridon <cristian.spiridon@gmail.com>
2020-01-05 12:35:34 +0000

    added keyboard responder, so on keyboard show we move the elements up

--- LennyMessaging/View/onboarding/CreateUserView.swift
+++ LennyMessaging/View/onboarding/CreateUserView.swift
@@ -11,12 +11,13 @@ import SwiftUI
 struct CreateUserView: View {
     
     @ObservedObject var createUserVM = CreateUserViewModel()
+    @ObservedObject var keyboard = KeyboardResponder()
     
     var body: some View {
         VStack {
             /// Title label
             Text("Let's give you a face")
-                .font(.title)
+                .font(.system(size: 28, weight: .bold, design: Font.Design.default))
                 .foregroundColor(Color.pink)
                 .padding()
                 .multilineTextAlignment(.center)
@@ -26,13 +27,23 @@ struct CreateUserView: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(width:200, height:200)
                 .clipShape(Circle())
+                .padding([.top, .bottom], 30)
             
-            //input
+            // User name Input
             TextField("Enter your username", text: self.$createUserVM.name)
-                .font(.title)
-                .foregroundColor(Color.white)
-                .multilineTextAlignment(.center)
-                .padding([.top], 100)
+                                   .font(.title)
+                                   .foregroundColor(Color.white)
+                                   .multilineTextAlignment(.center)
+                                    .padding(.bottom, keyboard.currentHeight / 2)
+        
+            
+            // after we have a valid username let's show the continue button
+                Button(action: {}) {
+                               Text("Continue")
+                           }
+                .padding([.top], 60)
+                .disabled(self.createUserVM.name == "")
+            
             
         }.frame(minWidth: 9, idealWidth: .infinity, maxWidth: .infinity, minHeight: 0, idealHeight: .infinity, maxHeight: .infinity, alignment: .center)
             .background(Image("background")
